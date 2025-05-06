cc9ca4a899f738873d6ea8c2e5681430bdfce571
Valentina <vrudnitskaya@gmail.com>
2025-05-05 20:22:47 -0500

    add app routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  resources :users
+  resources :posts do
+    resources :post_tags, only: [:index, :create, :destroy]
+  end
+  resources :comments
+  resources :tags, only: [:index, :show, :create, :destroy]
+  resources :likes, only: [:index, :create, :destroy]
+  resources :bookmarks, only: [:index, :create, :destroy]
+  resources :follows, only: [:index, :create, :destroy]
+  resources :tag_follows, only: [:index, :create, :destroy]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
