7193fb63e98ca96e564f6a326994d66197aac964
Valentina <vrudnitskaya@gmail.com>
2025-05-17 21:11:24 -0500

    implement create-update routes for post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,54 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [ :show, :edit, :update ]
+
+  def new
+    @post = Post.new
+  end
+
+  # we temporary use the random user to create the post
+  def create
+    random_user = User.order("RANDOM()").first
+
+    unless random_user
+      redirect_to root_path, alert: 'No users found to assign this post.'
+      return
+    end
+
+    @post = Post.new(post_params)
+    @post.user = random_user
+
+    if @post.save
+      redirect_to @post, notice: 'Post was successfully created.'
+    else
+      render :new
+    end
+  end
+  
   def show
-    @post = Post.find(params[:id])
     @tags = @post.tags
     @like_count = @post.likes.count
     @bookmark_count = Bookmark.where(post_id: @post.id).count
     @comments = @post.comments.where(parent_comment_id: nil).includes(:user)
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: "Post was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content, :image_url)
   end
 end
