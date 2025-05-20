ee13d462a78ebbdd5001d50cbe51d7e47b40034a
Valentina <vrudnitskaya@gmail.com>
2025-05-19 21:45:58 -0500

    implement comments updating

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: [:edit, :update]
+  before_action :authorize_user!, only: [:edit, :update]
+
   def new
     @comment = Comment.new(
       parent_comment_id: params[:parent_comment_id],
@@ -7,9 +10,7 @@ class CommentsController < ApplicationController
   end
 
   def create
-    # temporary
-    user = User.order("RANDOM()").first
-
+    user = User.order("RANDOM()").first # after will be changed for current_user
     @comment = Comment.new(comment_params)
     @comment.user = user
 
@@ -20,7 +21,28 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to post_path(@comment.post_id), notice: "Comment updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def authorize_user!
+    unless @comment.user_id == @comment.user.id
+      redirect_to post_path(@comment.post_id), alert: "You can only edit your own comments."
+    end
+  end
 
   def comment_params
     params.require(:comment).permit(:content, :post_id, :parent_comment_id)
