13d4ad1aa2599d0d97cbffbdc83de7f6fc0592c2
Valentina <vrudnitskaya@gmail.com>
2025-05-31 20:54:24 -0500

    implement authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
-  validates :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true, length: { minimum: 3 }
+  validates :password, length: { minimum: 5 }
   has_many :posts
   has_many :comments
   has_many :bookmarks
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root "home#index"
+  get "/signup", to: "registrations#new"
+  post "/signup", to: "registrations#create"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   resources :users do
     member do
       get "followers"
