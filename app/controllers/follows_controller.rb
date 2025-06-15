dca1da1d48a71b9734f8d275b3337b5d033bbefa
Valentina <vrudnitskaya@gmail.com>
2025-06-14 22:25:36 -0500

    change flashes styles, add flashed to follows controller

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -4,13 +4,13 @@ class FollowsController < ApplicationController
   def create
     followed_user = User.find(params[:followed_user_id])
     current_user.following_relationships.create!(followed_user: followed_user)
-    redirect_back(fallback_location: user_path(followed_user))
+    redirect_back(fallback_location: user_path(followed_user), notice: "Started following #{followed_user.username}.")
   end
 
   def destroy
     followed_user = User.find(params[:id])
     follow = current_user.following_relationships.find_by(followed_user: followed_user)
     follow&.destroy
-    redirect_back(fallback_location: user_path(followed_user))
+    redirect_back(fallback_location: user_path(followed_user), notice: "Unfollowed #{followed_user.username}.")
   end
 end
